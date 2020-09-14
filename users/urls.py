ad0e167589be6388df4e6ceef558469152755eb6
Zahra Salehi <zahrasalehii19988@gmail.com>
2020-09-14 17:57:16 +0430

    initial commit

---
+++ users/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url, include
+from .views import dashboard, register
+
+urlpatterns = [
+    url(r'^accounts/', include('django.contrib.auth.urls')),
+    url(r'^dashboard/', dashboard, name='dashboard'),
+    url(r'^register/', register, name='register')
+]
